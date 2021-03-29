9a7ded1ceb981157e59d6f556d87744c68b5ddd9
Maxim <masya.prozorov@mail.ru>
2021-03-29 12:08:16 +0000

    ModelUniversity

---
+++ Lab2/UniversityModel/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UniversityModelLib;
+
+namespace UniversityModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ListStudent = new List<string>();
+
+            string answer = "work";
+            ConsoleKeyInfo key;
+
+            OperationList NewStudent = new OperationList();
+            Console.WriteLine("Для ввода нового студента нажмите Enter, для выхода-Escape");
+            Console.WriteLine("F1-показать список, F2-очистить список");
+            while (answer == "work")
+            {
+                string Data = NewStudent.AddNewStudent();
+                Console.WriteLine(Data);
+                NewStudent.AddToList(Data, ListStudent);
+                key = Console.ReadKey();
+                ConsoleKey keyPress = key.Key;
+
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                };
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    answer = "work";
+                };
+
+                if (key.Key == ConsoleKey.F1)
+                {
+                    NewStudent.WriteList( ListStudent );
+                    key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    };
+                };
+
+                if (key.Key == ConsoleKey.F2)
+                {
+                    NewStudent.DeleteList( ListStudent );
+                    key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    };
+                };
+            };
+        }
+    }
+}
