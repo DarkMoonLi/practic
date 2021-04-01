f9cbbb347e53c02afed23672b2c10b635d0cbe2b
Maxim <masya.prozorov@mail.ru>
2021-04-01 22:58:54 +0000

    Внесены поправки в виде обработки клавиатуры

--- Lab2/UniversityModel/Program.cs
+++ Lab2/UniversityModel/Program.cs
@@ -9,51 +9,19 @@ namespace UniversityModel
         static void Main(string[] args)
         {
             var ListStudent = new List<string>();
-
-            string answer = "work";
-            ConsoleKeyInfo key;
-
             OperationList NewStudent = new OperationList();
+            KeyProcessing key_down = new KeyProcessing();
+
             Console.WriteLine("Для ввода нового студента нажмите Enter, для выхода-Escape");
             Console.WriteLine("F1-показать список, F2-очистить список");
-            while (answer == "work")
+            key_down.answer = "work";
+            while (key_down.answer == "work")
             {
                 string Data = NewStudent.AddNewStudent();
                 Console.WriteLine(Data);
                 NewStudent.AddToList(Data, ListStudent);
-                key = Console.ReadKey();
-                ConsoleKey keyPress = key.Key;
-
-                if (key.Key == ConsoleKey.Escape)
-                {
-                    break;
-                };
-
-                if (key.Key == ConsoleKey.Enter)
-                {
-                    answer = "work";
-                };
-
-                if (key.Key == ConsoleKey.F1)
-                {
-                    NewStudent.WriteList( ListStudent );
-                    key = Console.ReadKey();
-                    if (key.Key == ConsoleKey.Escape)
-                    {
-                        break;
-                    };
-                };
-
-                if (key.Key == ConsoleKey.F2)
-                {
-                    NewStudent.DeleteList( ListStudent );
-                    key = Console.ReadKey();
-                    if (key.Key == ConsoleKey.Escape)
-                    {
-                        break;
-                    };
-                };
-            };
+                key_down.KeyPress( key_down.answer, key_down, NewStudent, ListStudent );
+            }
         }
     }
 }
---
+++ Lab2/UniversityModelLib/KeyProcessing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityModelLib
+{
+    public class KeyProcessing : IKeyProcessing
+    {
+        public string answer;
+        ConsoleKeyInfo key;
+        public string KeyPress(string answer, KeyProcessing key_down, OperationList NewStudent, List<string> ListStudent)
+        {
+            key = Console.ReadKey();
+            ConsoleKey PressKey = key.Key;
+
+            switch (key.Key)
+            {
+                case ConsoleKey.Escape:
+                    {
+                        key_down.answer = "don't work";
+                        return key_down.answer;
+                    };
+
+                case ConsoleKey.Enter:
+                    {
+                        key_down.answer = "work";
+                        return key_down.answer;
+                    };
+
+                case ConsoleKey.F1:
+                    {
+                        NewStudent.WriteList(ListStudent);
+                        key_down.KeyPress(answer, key_down, NewStudent, ListStudent);
+                        return key_down.answer;
+                    };
+
+                case ConsoleKey.F2:
+                    {
+                        NewStudent.DeleteList(ListStudent);
+                        key_down.KeyPress(answer, key_down, NewStudent, ListStudent);
+                        return key_down.answer;
+                    };
+            }
+            key_down.answer = "don't work";
+            return key_down.answer;
+        }
+    }
+}
