928f34c44a65219e58db92d31374ff8606af22ba
DarkMoonLi <45242101+DarkMoonLi@users.noreply.github.com>
2021-04-12 10:57:30 +0000

    удален комментарий, где была проблема

--- ShopLab/Program.cs
+++ ShopLab/Program.cs
@@ -151,7 +151,7 @@ namespace ShopLab
                                 Name = Convert.ToString(reader["Name"]),
                                 CountOrder = Convert.ToInt32(reader["CountOrder"]),
                                 SumPrice = Convert.ToInt32(reader["SumPrice"])
-                            };                                                      //проблемы с выводом данных
+                            };                                                     
                             statistics.Add(statisticCustomer);
                         }
                     }
